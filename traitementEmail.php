ac3e6fa4385511e9007bebab1332d77fd1447502
gamcoh <gcohen@adventureconseil.com>
2016-07-29 11:23:50 +0200

    Update

--- traitementEmail.php
+++ traitementEmail.php
@@ -1,18 +1,77 @@
 <?php 
-// the email recupered from the user
-$email = $_POST['email'];
+// connection to database
+$db = new PDO('mysql:host=YOUR_HOST;dbname=YOUR_DBNAME;charset=utf8', 'USER', 'PASSWORD');
 
-// the existing webmails
-$webmails = ["hotmail","orange","gmail","yahoo","sfr","live","wanadoo","laposte","free","neuf","voila","aol","msn","bbox","aliceadsl","numericable","dbmail","cegetel","gmx","outlook","ymail","akeonet","netcourrier","noos","libertysurf","estvideo","dartybox","nordnet","mail","rocketmail"];
+// email domains known
+$domains = array(
+	"hotmail",
+	"orange",
+	"gmail",
+	"yahoo",
+	"sfr",
+	"live",
+	"wanadoo",
+	"laposte",
+	"free",
+	"neuf",
+	"voila",
+	"aol",
+	"msn",
+	"bbox",
+	"aliceadsl",
+	"numericable",
+	"dbmail",
+	"cegetel",
+	"gmx",
+	"outlook",
+	"ymail",
+	"akeonet",
+	"netcourrier",
+	"noos",
+	"libertysurf",
+	"estvideo",
+	"dartybox",
+	"nordnet",
+	"mail",
+	"rocketmail"
+);
 
-// we get the webmail from the user's email
-preg_match("/@([^\.]*)/i", $email, $matches);
+// get the domain name from all emails in my table
+$req = "SELECT SUBSTRING_INDEX(SUBSTRING_INDEX(member_email, '@', -1), '.', 1) AS domain_name, COUNT(member_email) AS nbr_domain
+FROM member GROUP BY domain_name";
 
-// we loop on the webmails array in order to find the right webmail
-foreach ($webmails as $webmail) {
-	// we can find the right webmail with the levenshtein function https://secure.php.net/manual/en/function.levenshtein.php
-	if (levenshtein($webmail, $matches[1]) < 2) {
-		// the corrected webmail
-		echo $webmail;
+// PDO query
+$res = $db->query($req);
+
+// fetch all
+$wrongDomainsWithCorrection=array();
+while ($data = $res->fetch(PDO::FETCH_OBJ)) {
+
+	// if the domain we're looking at
+	// is already in the domains array
+	// that means it is a good one
+	// so we don't need to search for his correction
+	if (in_array($data->domain_name, $domains)) {
+		continue;
 	}
+
+	// else we loop on the domains 
+	// array in order to see if
+	// the wrong domains have a 
+	// potential correction
+	foreach ($domains as $domain) {
+
+		// go in php Manual to see the levenshtein
+		// https://secure.php.net/manual/fr/function.levenshtein.php
+		if ( levenshtein($domain, $data->domain_name) < 2 ) {
+			// the array we return is formatted like that:
+			// "wrongdomain" = 10 => "correctededdomain"
+			$wrongDomainsWithCorrection[$data->domain_name] = array("number_occurence" => $data->nbr_domain, "domain_corrected" => $domain);
+		}
+	}
+
 }
+
+return $wrongDomainsWithCorrection;
+
+
